d8a9784790a2224d7ce4e932cae8ac55a2b17d93
ZeNoobi <ctaaseen@gmail.com>
2023-04-10 23:58:02 -0700

    idGeneration

---
+++ id.js
@@ -0,0 +1,11 @@
+const idBTN = document.getElementById('idBTN');
+const idDisplay = document.getElementById('idDisplay');
+const hiddenID = document.getElementById('hiddenID');
+
+idBTN.addEventListener("click", ()=>{
+    const id = Math.random().toString(36).substring(2, 10).toUpperCase();
+    idDisplay.textContent=id;
+    hiddenID.value=id;
+});
+
+//event.preventDefault();
