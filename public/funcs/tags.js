0c9ae2c10cde3c062fb600453f755118647b0c14
ZeNoobi <ctaaseen@gmail.com>
2023-05-05 18:56:33 -0700

    JsonData Successfully sending Web Browser Console

--- public/funcs/tags.js
+++ public/funcs/tags.js
@@ -1,8 +1,42 @@
+/*
+const fs = require('fs');
+
+const readDataFile = fs.readFileSync('./data/reqnonfunc.json', 'utf-8', (error) =>{
+  if(error){
+      console.log(error);
+      return;
+  }    
+});
+
+console.log(readDataFile);
+console.log("Read Data File^");
+
+let parsedData = JSON.parse(readDataFile);
+
+console.log(parsedData);
+console.log("parsed Data^");
+
+console.log("Below is for loop");
+for(let i = 0; i<parsedData.length; i++){
+  console.log(parsedData[i]);
+}
+*/
+
+
 // Initialize array to hold chosen requirements
 let chosenRequirements = [];
 
-// Add event listeners to each tag in avareqtags div
 const avareqtags = document.querySelector('.avareqtags');
+
+// Add event listeners to each tag in avareqtags div
+/*
+for(let i = 0; i<parsedData.length; i++){
+  const newtagfromJSON = document.createElement('span');
+  newtagfromJSON.className = 'reqtag';
+  newtagfromJSON.innerHTML = parsedData[i];
+  avareqtags.appendChild(newtagfromJSON);
+}
+*/
 avareqtags.addEventListener('click', function(event) {
   if (event.target.tagName === 'SPAN') {
     // Remove clicked tag from avareqtags div
---
+++ public/loadreqs.js
@@ -0,0 +1,40 @@
+//const fs = require('fs');
+//import * as fs from 'fs'; 
+
+//const { response } = require("express");
+
+console.log("loadreq Commenced");
+
+await fetch('reqnonfunc.json')
+.then(response => response.json())
+.then(data => {console.log(data);})
+
+
+console.log("End of Fetch");
+
+
+
+/*
+const readDataFile = fs.readFileSync('./data/reqnonfunc.json', 'utf-8', (error) =>{
+  if(error){
+      console.log(error);
+      return;
+  }    
+});
+
+console.log(readDataFile);
+
+
+console.log("Read Data File^");
+let parsedData = JSON.parse(readDataFile);
+
+console.log(parsedData);
+console.log("parsed Data^");
+
+console.log("Below is for loop");
+for(let i = 0; i<parsedData.length; i++){
+  console.log(parsedData[i]);
+}
+*/
+
+
